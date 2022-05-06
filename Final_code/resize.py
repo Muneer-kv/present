c748edbec5aab206a4d5dc1d13c8496012385ebd
muneer-kv <muneerkvzz@gmail.com>
2022-05-06 15:58:03 +0530

    final sub

---
+++ Final_code/resize.py
@@ -0,0 +1,22 @@
+
+import PIL
+import os
+import os.path
+from PIL import Image
+
+f = r'C:\Users\ja6viz\Desktop\muneer\Final_code\train\8'
+for file in os.listdir(f):
+    f_img = f+"/"+file
+    img = Image.open(f_img)
+    img = img.convert("RGB")
+    img = img.resize((32,32))
+    img.save(f_img)
+#################################    RENAME  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  
+##import os
+##path = path=r"C:\Users\ja6viz\Desktop\muneer\Final_code\train\10"
+##files = os.listdir(path)
+##
+##
+##for index, file in enumerate(files):
+##    os.rename(os.path.join(path, file), os.path.join(path, ''.join([str(index), '.jpg'])))
---
+++ firstapp/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from firstapp import views
+urlpatterns = [
+    path('index',views.index,name='index'),
+    path('login.html',views.login,name='login'),
+    path('bmi.html',views.bmic,name='bmi'),
+    path('register.html',views.register,name='register'),
+    path('',views.welcome,name='welcome'),
+    path('welcome.html',views.welcome,name='logout'),
+    path('home',views.index,name='home'),
+    path('calorie',views.calorie,name='calorie'),
+    path('apc',views.apc,name='apc')
+
+]
